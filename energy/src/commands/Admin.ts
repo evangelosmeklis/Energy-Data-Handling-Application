ef8bfe8f53184d586023285d5f55b7254a2912fb
Vangelis Meklis <evangelosmeklis@windowslive.com>
2020-01-18 14:25:53 +0200

    almost finished admin

--- energy/src/commands/Admin.ts
+++ energy/src/commands/Admin.ts
@@ -6,13 +6,13 @@ import {flags} from  '@oclif/command'
 
 export class AdminCommand extends Command {
   static flags = { 
-    newuser: flags.string({dependsOn:['passw'],exclusive:['moduser']}),
-    moduser: flags.string({dependsOn:['passw'],exclusive:['newuser']}),
+    newuser: flags.string({dependsOn:['passw'],exclusive:['newdata','userstatus','moduser']}),
+    moduser: flags.string({dependsOn:['passw'],exclusive:['newuser','newdata','userstatus']}),
     passw: flags.string({dependsOn:['email']}),
     email: flags.string({dependsOn:['quota']}),
     quota: flags.string(),
-    userstatus: flags.string(),
-    newdata: flags.string({dependsOn:['source']}),
+    userstatus: flags.string({exclusive:['newdata','newuser','moduser']}),
+    newdata: flags.string({dependsOn:['source'],exclusive:['newdata','newuser','moduser']}),
     source: flags.string()
   }
   async run() {
@@ -37,6 +37,21 @@ export class AdminCommand extends Command {
 			if (`${flags.newuser}` !== "undefined" && `${flags.passw}` !== "undefined" && `${flags.email}` !== "undefined" && `${flags.quota}` !== "undefined" ){
 			    //console.log("inside");
 				await connection.query(`INSERT INTO users VALUES (?,?,?,?)`,[`${flags.newuser}`,`${flags.passw}`,`${flags.email}`,`${flags.quota}`]);
+				console.log("Succesful");
+			}
+			else if (`${flags.moduser}` !== "undefined" && `${flags.passw}` !== "undefined" && `${flags.email}` !== "undefined" && `${flags.quota}` !== "undefined" ){
+				await connection.query(`UPDATE users SET  pass=?,email=?,quota=? WHERE user=?  `,[`${flags.passw}`,`${flags.email}`,`${flags.quota}`,`${flags.moduser}`]);
+				console.log("Succesful");
+			}
+			else if (`${flags.userstatus}` !== "undefined"){
+				const ret=await connection.query(`SELECT user,email,quota FROM users WHERE user=?`,[`${flags.userstatus}`],function(err,result,fields){
+					if (err) throw err;
+					console.log(result);
+				});
+			}
+			else if (`${flags.newdata}` !== "undefined" && `${flags.source}` !== "undefined" ){
+				await connection.query(`LOAD DATA LOCAL INFILE ? CHARACTER SET UTF8 INTO TABLE ?  FIELDS TERMINATED BY ';' LINES TERMINATED BY '\r\n' 
+					IGNORE 1 LINES;`,[`${flags.source}`,`${flags.source}`]);
 				console.log("Succesful");
 			}
 			else console.log("Unsuccesful" + `${flags.newuser}`,`${flags.pass}`,`${flags.email}`,`${flags.quota}`);
